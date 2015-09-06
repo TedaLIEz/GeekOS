b143bebf2b2fd2d58618d5b371b8851d737041ee
Neil Spring <nspring@cs.umd.edu>
2015-09-06 09:04:33 -0400

    remove extra cruft

--- src/user/overstck.c
+++
@@ -1,7 +0,0 @@
-#include <conio.h>
-
-int main() {
-    int *i = (int *)0x70000000;
-    Print("I should not print %d\n", *i);
-    return 0;
-}
--- src/user/rodata.c
+++
@@ -1,7 +0,0 @@
-#include <conio.h>
-int main() {
-    const char *foo = "abcdefg";
-    ((char *)foo)[0] = 'q';
-    Print("FAIL: should have aborted before getting here.\n");
-    return 1;
-}
